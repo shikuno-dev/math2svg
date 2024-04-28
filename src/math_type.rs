b12db41d9abcf65bfc904c9175fc0f84e71eb8c4
shikuno-dev <shir01earn@gmail.com>
2024-04-28 21:13:35 +0900

    initial commit

---
+++ src/math_type.rs
@@ -0,0 +1,4 @@
+// mi: identifier (Examples: variable, function name, symbolic constant)
+// mn: numeric literal (Examples: integer, decimal number)
+// mo: operator (Examples: binary operation, binary relation, summation symbol)
+// mtext: generic text (Examples: short words in formulas )
