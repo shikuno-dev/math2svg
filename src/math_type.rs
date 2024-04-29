426eb694023348ab83d69f06b4f38df1d9eda886
shikuno-dev <shir01earn@gmail.com>
2024-04-29 20:07:51 +0900

    initial commit

--- src/math_type.rs
+++ src/math_type.rs
@@ -2,3 +2,25 @@
 // mn: numeric literal (Examples: integer, decimal number)
 // mo: operator (Examples: binary operation, binary relation, summation symbol)
 // mtext: generic text (Examples: short words in formulas )
+
+// mrow
+// msub
+// msup
+// msubsup
+// munder
+// mover
+// munderover
+
+// mtable
+// mtr
+// mtd: columnspan, rowspan
+
+enum MathType {
+    Identifier, // Symbols like pi, dot, or RR
+    Numeric,
+    Operator,
+    Variables, // Single letters are always displayed as is.
+               // Multiple letters, however, are interpreted as variables and functions.
+               // To display multiple letters verbatim, you can place them into quotes
+               // and to access single letter variables, you can use the hash syntax.
+}
