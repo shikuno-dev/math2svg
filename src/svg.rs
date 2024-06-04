a9920dbe996f763622752dbe77e653dfe330e24a
shikuno-dev <shir01earn@gmail.com>
2024-06-04 23:53:39 +0900

    initial commit

--- src/svg.rs
+++ src/svg.rs
@@ -1,9 +1,10 @@
-pub fn svg_header(width: usize, height: usize) -> String {
+pub fn svg_header(width: usize, height: usize, fill: &str) -> String {
     format!(
         r#"<svg height="{height}" width="{width}" xmlns="http://www.w3.org/2000/svg">
-<rect width="100%" height="100%" fill="white"/>"#,
+<rect width="100%" height="100%" fill="{fill}"/>"#,
         width = width,
-        height = height
+        height = height,
+        fill = fill
     )
 }
 
